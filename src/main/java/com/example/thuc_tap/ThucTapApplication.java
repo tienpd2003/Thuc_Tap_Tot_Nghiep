d4659486908627c0c12a35be33d51e1eadf00b4a
Admin <tienpd2003@gmail.com>
2025-08-12 14:13:47 +0700

    first commit

---
+++ src/main/java/com/example/thuc_tap/ThucTapApplication.java
@@ -0,0 +1,13 @@
+package com.example.thuc_tap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThucTapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThucTapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thuc_tap/ThucTapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thuc_tap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThucTapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
